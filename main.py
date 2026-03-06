c09f05f595a633cccd880dfd48f3e31bd69d23e2
Abhishek <dhokareabhi@gmail.com>
2026-03-06 11:41:27 +0530

    Initial commit - Multi courier billing system

--- main.py
+++ main.py
@@ -28,8 +28,18 @@ from services.trackon_west_service import (
     calculate_trackon_west_rate,
     load_all_trackon_west_rates
 )
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
